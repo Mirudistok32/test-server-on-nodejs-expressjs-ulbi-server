cf740e2a73a9f868e736ec29ee0c117bddee79d6
Stanislav Postnikov <mirudistok32@yandex.ru>
2020-12-12 14:53:28 +0300

    Create login

--- authController.js
+++ authController.js
@@ -1,7 +1,18 @@
 const User = require("./models/User");
 const Role = require("./models/Role");
-const bcrypt = require("bcryptjs");
 const { validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { secret } = require("./config");
+
+const generateAccessToken = (id, roles) => {
+  const payload = {
+    id,
+    roles,
+  };
+
+  return jwt.sign(payload, secret, { expiresIn: "12h" });
+};
 
 class authController {
   async registration(req, res) {
@@ -37,6 +48,21 @@ class authController {
   }
   async login(req, res) {
     try {
+      const { username, password } = req.body;
+      const user = await User.findOne({ username });
+
+      if (!user) {
+        return res.status(400).json({ message: `User ${username} not found` });
+      }
+
+      const validPassword = bcrypt.compareSync(password, user.password);
+      if (!validPassword) {
+        return res.status(400).json({ message: `Invalid password` });
+      }
+
+      const token = generateAccessToken(user._id, user.roles);
+
+      return res.json({ token, message: `Successe!` });
     } catch (error) {
       console.log(error);
       res.status(400).json({ message: `Login error` });
---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    secret: "RANDOMNIY_KEY"
+}
