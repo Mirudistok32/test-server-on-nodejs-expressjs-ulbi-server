c13996a19ff5573d171fc2c8b95e35ec775e4686
Stanislav Postnikov <mirudistok32@yandex.ru>
2020-12-12 14:02:55 +0300

    Create registration user

--- authController.js
+++ authController.js
@@ -1,22 +1,38 @@
-const User = require('./models/User')
-const Role = require('./models/Role')
+const User = require("./models/User");
+const Role = require("./models/Role");
+const bcrypt = require("bcryptjs");
 
 class authController {
   async registration(req, res) {
     try {
+      const { username, password } = req.body;
+      const candidate = await User.findOne({ username });
+
+      if (candidate) {
+        return res.status(400).json({ message: `User already exists!` });
+      }
+
+      const hashPassword = bcrypt.hashSync(password, 7);
+      const userRole = await Role.findOne({ value: "USER" });
+      const user = new User({ username, password: hashPassword, roles: [userRole.value] });
+      await user.save()
+
+      return res.json({message: `User was successed created!`})
     } catch (error) {
       console.log(error);
+      res.status(400).json({ message: `Registration error` });
     }
   }
   async login(req, res) {
     try {
     } catch (error) {
       console.log(error);
+      res.status(400).json({ message: `Login error` });
     }
   }
   async getUsers(req, res) {
     try {
-      res.json("Server Users")
+      res.json("Server Users");
     } catch (error) {
       console.log(error);
     }
