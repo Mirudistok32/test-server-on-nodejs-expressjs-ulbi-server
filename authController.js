5b124fcab60da50af1c3f2c39efcf1f2350c52bf
Stanislav Postnikov <mirudistok32@yandex.ru>
2020-12-12 14:19:26 +0300

    Create validation for registration

--- authController.js
+++ authController.js
@@ -1,10 +1,18 @@
 const User = require("./models/User");
 const Role = require("./models/Role");
 const bcrypt = require("bcryptjs");
+const { validationResult } = require("express-validator");
 
 class authController {
   async registration(req, res) {
     try {
+      const errorsValid = validationResult(req);
+      if (!errorsValid.isEmpty()) {
+        return res
+          .status(400)
+          .json({ message: `Error in registration`, errors: errorsValid });
+      }
+
       const { username, password } = req.body;
       const candidate = await User.findOne({ username });
 
@@ -14,10 +22,14 @@ class authController {
 
       const hashPassword = bcrypt.hashSync(password, 7);
       const userRole = await Role.findOne({ value: "USER" });
-      const user = new User({ username, password: hashPassword, roles: [userRole.value] });
-      await user.save()
+      const user = new User({
+        username,
+        password: hashPassword,
+        roles: [userRole.value],
+      });
+      await user.save();
 
-      return res.json({message: `User was successed created!`})
+      return res.json({ message: `User was successed created!` });
     } catch (error) {
       console.log(error);
       res.status(400).json({ message: `Registration error` });
--- authRouter.js
+++ authRouter.js
@@ -1,9 +1,19 @@
 const Router = require("express");
 const router = new Router();
 const controller = require("./authController");
+const { check } = require("express-validator");
 
-
-router.post("/registration", controller.registration);
+router.post(
+  "/registration",
+  [
+    check("username", `Username don't be is empty and should be more than 4 symbols`).notEmpty().isLength({min: 4}),
+    check(
+      "password",
+      `Password should be more than 4 symbols and less than 10 symbols`
+    ).isLength({ min: 4, max: 10 }),
+  ],
+  controller.registration
+);
 router.post("/login", controller.login);
 router.get("/users", controller.getUsers);
 
